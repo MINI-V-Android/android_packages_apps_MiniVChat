a01317eb24ce0801e23567746ac7aba981799130
viiniu <11e26db@gmail.com>
2026-09-09 16:37:00 +0900

    HAL 인터페이스에 추가된 메서드 정의를 추가 했더용

--- java/com/miniv/chat/activity/MainActivity.kt
+++ java/com/miniv/chat/activity/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.WindowInsets
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
